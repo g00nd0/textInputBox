4325ba408a8639c43f0298b9690f7094ad9584e9
g00nd0 <g00nd0@MitchT495s.localdomain>
2021-03-04 17:06:05 +0800

    Render text output with color changes

--- client-react/src/components/textDisplay/TextOutput.js
+++ client-react/src/components/textDisplay/TextOutput.js
@@ -1,12 +1,40 @@
 import React from "react";
 
-// const inputText = "Typed Text Will Be Displayed Here";
-
 const TextOutput = ({ text, defaultText }) => {
-  return (
-    <p>
-      <b>{text === "" ? defaultText : text}</b>
-    </p>
-  );
+  const renderDefaultText = (inputText) => {
+    return (
+      <p>
+        <b>{inputText}</b>
+      </p>
+    );
+  };
+
+  const renderText = (inputText) => {
+    let textColor;
+    switch (true) {
+      case inputText.length < 4:
+        textColor = "red";
+        break;
+      case inputText.length >= 4 && inputText.length < 8:
+        textColor = "yellow";
+        break;
+      case inputText.length >= 8 && inputText.length < 13:
+        textColor = "black";
+        break;
+      case inputText.length >= 13 && inputText.length < 16:
+        textColor = "green";
+        break;
+      default:
+        textColor = "blue";
+    }
+
+    return (
+      <p style={{ color: textColor }}>
+        <b>{inputText}</b>
+      </p>
+    );
+  };
+
+  return <>{text === "" ? renderDefaultText(defaultText) : renderText(text)}</>;
 };
 export default TextOutput;
